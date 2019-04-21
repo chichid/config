1a10e1b7f0b8d766bcf0844b9656021fb787f5b1
Rachid El Habbari <rachid.el.habbari@oracle.com>
2019-04-21 14:26:28 -0400

    Configurations

---
+++ .vimrc
@@ -0,0 +1,132 @@
+"""""""""""""
+" Core
+""""""""""""""
+execute pathogen#infect()
+
+"""""""""""
+" General
+""""""""""""
+filetype plugin indent on
+syntax on
+set nocompatible
+set backspace=indent,eol,start
+set whichwrap+=<,>,h,l,[,]
+set number              
+set wrap!                
+set encoding=utf-8     
+set wildmenu
+set showmatch
+set ruler  
+set incsearch
+set hlsearch
+set autoread          
+set linebreak
+set nolist 
+set textwidth=0
+set wrapmargin=0 
+set history=1000 
+set ignorecase
+set smartcase
+set laststatus=0
+
+"""""""""""""""
+" Indent
+"""""""""""""""
+set tabstop=2
+set expandtab
+set shiftwidth=2
+set autoindent
+set smartindent
+autocmd VimEnter * DetectIndent
+
+"""""""""""""""
+" Keyboard Mapping
+"""""""""""""""
+nmap q :qa<CR>
+nnoremap <silent> <BS> i<BS><ESC> 
+noremap <silent> <CR> :nohlsearch<CR>
+nnoremap <silent> <expr> <RIGHT> g:NERDTree.IsOpen() ? ":call MyNERDTreeOpenNode()<CR>" : "<RIGHT>"
+nnoremap <silent> <expr> <LEFT> g:NERDTree.IsOpen() ? ":call MyNERDTreeCloseNode()<CR>" : "<LEFT>"
+nnoremap <silent> <C-n> :NERDTreeToggle<CR>
+nnoremap \ i<CR><ESC>
+nnoremap <BAR> o<ESC><UP>
+nnoremap <S-RIGHT> $
+nnoremap <S-LEFT> 0
+nnoremap < <<
+nnoremap > >>
+vnoremap > >gv
+vnoremap < <gv
+
+""""""""""""""
+" Ctrl-p 
+""""""""""""""
+
+""""""""""""""
+" NERDTree
+""""""""""""""
+let g:NERDTreeMinimalUI = 1
+let g:NERDTreeDirArrowExpandable = '▹'
+let g:NERDTreeQuitOnOpen = 1
+
+autocmd StdinReadPre * let s:std_in=1
+autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in") | exe 'NERDTree' argv()[0] | wincmd p | ene | exe 'cd '.argv()[0] | call MyNERDTreeOpenDir() | endif
+autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
+
+function! MyNERDTreeOpenDir()
+	execute ":NERDTreeFocus"
+	
+	for child in g:NERDTreeFileNode.GetRootForTab().children
+		if has_key(child, "path") && child.path.isDirectory !=# 1 && child.isVisible() ==# 1
+			call child.putCursorHere(0, 0)
+			call timer_start(1, { tid -> execute("normal go") })
+			break
+		endif
+	endfor
+endfunction
+
+function! MyNERDTreeOpenNode()
+	let l:sf = g:NERDTreeFileNode.GetSelected()
+	if has_key(l:sf, "path")
+		if l:sf.path.isDirectory ==# 1 && l:sf.isOpen ==# 0 
+			call l:sf.activate()
+		elseif l:sf.path.isDirectory !=# 1 
+			call l:sf.activate()
+		endif
+	endif
+endfunction
+
+function! MyNERDTreeCloseNode()
+	let l:sf = g:NERDTreeFileNode.GetSelected()
+	if has_key(l:sf, "path")
+		if l:sf.path.isDirectory ==# 1 && l:sf.isOpen ==# 1
+			call l:sf.activate()
+		else
+			call l:sf.parent.putCursorHere(0, 0)
+		endif
+	endif
+endfunction
+
+""""""""""""""
+" Theme
+""""""""""""""
+set cursorline
+
+highlight LineNr cterm=NONE ctermbg=0 ctermfg=241
+highlight StatusLine ctermbg=0
+highlight CursorLine cterm=NONE ctermbg=8 
+highlight CursorLineNr cterm=bold ctermbg=8 ctermfg=7
+highlight VertSplit ctermfg=0 ctermbg=0
+highlight MatchParen term=NONE ctermbg=LightGrey ctermfg=0
+highlight Visual term=NONE ctermbg=11 ctermfg=0
+highlight EndOfBuffer ctermfg=0
+highlight Search term=NONE cterm=NONE ctermfg=0
+highlight NERDTreeClosable ctermfg=11 
+
+highlight Statement ctermfg=11
+highlight Identifier ctermfg=6
+highlight String ctermfg=10
+highlight Character ctermfg=10
+highlight Number ctermfg=10 
+highlight Boolean ctermfg=10 
+highlight Float ctermfg=10
+
---
+++ Scripts/fnd-apps/qunit-test-main.js
@@ -0,0 +1,143 @@
+window.vbInitConfig = {
+	SERVICE_WORKER_CONFIG: {
+		disabled: true
+	}
+};
+
+var allCCALoaders = [];
+var commonTestFiles = [];
+// var TEST_REGEXP = /(-test)\.js$/i
+
+// check if the test file is present in any of the directory or sub directory of the packages to be tested.
+function isAddTestFile(testSuiteFile) {
+	var testFile = false;
+	// check if any packages are passed as arguments to the karma start command.
+	if (window.__karma__.config.args && window.__karma__.config.args.length > 0) {
+		// If all is present as only argument then test all the suites.
+		if (window.__karma__.config.args[0] === 'all') {
+			testFile = true;
+		} else {
+			// else for each package check if it is part of file, if yes then execute the test suite.
+			for (var i = 0; i < window.__karma__.config.args.length; i++) {
+				if (testSuiteFile.indexOf(window.__karma__.config.args[i]) >= 0) {
+					testFile = true;
+					break;
+				}
+			}
+		}
+	} else {
+		// If no packages are defined then execute all the test suites.
+		testFile = true;
+	}
+	// console.log('Run Tests in Test suite: ' + testSuiteFile + " : " + testFile);
+	return testFile;
+}
+
+Object.keys(window.__karma__.files).forEach(function(file) {
+	if (file.endsWith('loader.js') && file.indexOf('/js/jet-composites') >= 0) {
+		console.log('Adding CCA Loader ' + file);
+		allCCALoaders.push(file);
+	}
+	if (file.indexOf('-test.js') >= 0 && file.indexOf('/js/jet-composites') >= 0 && file.indexOf('/qunit/') >= 0 && isAddTestFile(file)) {
+		console.log('Run tests in test suite ' + file);
+		commonTestFiles.push(file);
+	}
+});
+
+allCCALoaders = allCCALoaders.map(loader => loader.replace('/base/src/js/jet-composites', 'oj-approvals').replace('.js', ''));
+
+require.config({
+	// Karma serves files under /base, which is the basePath from your config file
+	//  baseUrl: '/base',
+	waitSeconds: 0,
+	paths: {
+		'oj-approvals': '/base/src/js/jet-composites',
+		'app-flow': '/base/tests/fixture/app-flow',
+		underscore: '/base/node_modules/underscore/underscore',
+		pouchdb: '/base/node_modules/@oracle/oraclejet/dist/js/libs/opt/min/pouchdb-browser-6.3.4',
+		'oj-dynamic': 'https://static-stage.oracle.com/cdn/jet/packs/oj-dynamic/6.0.0-alpha.1.1'
+	}
+});
+
+Promise.all([
+	requirePromise(allCCALoaders, 'CCA Loaders'),
+	requirePromise(commonTestFiles, 'Common Test Files')
+]).then(() => {
+	console.log('All resources have been loaded starting karma...');
+	window.__karma__.start();
+});
+
+function requirePromise(deps, resource) {
+	console.log('Loading resource ' + resource);
+
+	return new Promise((r, f) => require(
+		deps,
+		() => {
+			console.log('Resource Loaded: ' + resource);
+			r();
+		}, (error) => {
+			console.error('Unable to require ' + resource);
+			console.error(error);
+			f();
+		}
+	));
+}
+
+const Colors = {
+  Reset: "\x1b[0m",
+  Bright: "\x1b[1m",
+  Dim: "\x1b[2m",
+  Underscore: "\x1b[4m",
+  Blink: "\x1b[5m",
+  Reverse: "\x1b[7m",
+  Hidden: "\x1b[8m",
+  fg: {
+   Black: "\x1b[30m",
+   Red: "\x1b[31m",
+   Green: "\x1b[32m",
+   Yellow: "\x1b[33m",
+   Blue: "\x1b[34m",
+   Magenta: "\x1b[35m",
+   Cyan: "\x1b[36m",
+   White: "\x1b[37m",
+   Crimson: "\x1b[38m" //القرمزي
+  },
+  bg: {
+   Black: "\x1b[40m",
+   Red: "\x1b[41m",
+   Green: "\x1b[42m",
+   Yellow: "\x1b[43m",
+   Blue: "\x1b[44m",
+   Magenta: "\x1b[45m",
+   Cyan: "\x1b[46m",
+   White: "\x1b[47m",
+   Crimson: "\x1b[48m"
+  }
+};
+
+QUnit.testStart(function( details ) {
+  var message = Colors.bg.White + Colors.Bright + ' NOW TESTING ' + '\x1b[49m' + ' ' + details.module + "-" + details.name;
+  console.info(message + Colors.Reset);
+});
+
+QUnit.testDone(function( details ) {	
+  var testMessage = details.module + "-" + details.name;
+  
+  var statusColor, statusText;
+  statusColor = Colors.bg.Green;
+  statusText =   "  PASSED  ";
+ 
+  if (details.failed > 0) {
+    statusColor = Colors.bg.Red;
+    statusText = "  FAILED  ";
+  } else if (details.skipped) {
+    statusColor = Colors.bg.White;
+    statusText = "  SKIPPED ";
+  } else if (details.todo) {
+    statusColor = Color.bg.Yellow;
+    statusText = "   TODO   ";
+  }
+
+  var message = statusColor + ' ' + '\x1b[1m' + statusText + ' ' + '\x1b[49m' + ' ' + testMessage;
+  console.info(message + Colors.Reset);
+});
