ca27ab8362a68c89a045849eceda50753bbb0548
Rachid El Habbari <rachid.el.habbari@gmail.com>
2020-12-16 18:45:32 -0500

    Added Windows Node Alias

--- fish/config.fish
+++ fish/config.fish
@@ -8,11 +8,12 @@ set -x LS_COLORS "$LS_COLORS:ow=1;34:tw=1;34:"
 #Key Bindings
 bind \el 'clear; commandline -f repaint'
 bind \ek 'clear; commandline -f repaint'
-bind \t forward-word
+# bind \t forward-word
 bind \e\[F\x7F backward-kill-word
 
 #Windows
 if string match -qa -- "*Microsoft*" (uname -a)
   alias npm "cmd.exe /c npm"
+  alias node "cmd.exe /c node"
 end
 
