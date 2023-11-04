597d2c9d084f305f9990f21046efd157b00c3aa2
chichid <rachid.el.habbari@gmail.com>
2023-11-04 17:16:17 -0400

    OMF

---
+++ fish/conf.d/omf.fish
@@ -0,0 +1,8 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH '/Users/dox/.config/fish/omf/plugin'
+
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG '/Users/dox/.config/fish/omf/config'
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
